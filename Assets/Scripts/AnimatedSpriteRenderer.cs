6c50c8361ebd58ee45b3b98703a29c020e95f09d
Dyeresko <denis.yeresko@gmail.com>
2023-03-03 16:22:25 +0200

    Bomberman

---
+++ Assets/Scripts/AnimatedSpriteRenderer.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class AnimatedSpriteRenderer : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public float animationTime = 0.25f;
+    private int animationFrame;
+    public Sprite[] animationSprites;
+    public Sprite idleSprite;
+    public bool loop = true;
+    public bool idle = true;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable()
+    {
+        spriteRenderer.enabled = true;
+    }
+    private void OnDisable()
+    {
+        spriteRenderer.enabled = false;
+    }
+    private void Start()
+    {
+        InvokeRepeating(nameof(NextFrame), animationTime, animationTime);
+    }
+    private void NextFrame()
+    {
+        animationFrame++;
+        if (loop && animationFrame > animationSprites.Length)
+        {
+            animationFrame = 0;
+        }
+        if (idle)
+        {
+            spriteRenderer.sprite = idleSprite;
+        }
+        else if (animationFrame >= 0 && animationFrame < animationSprites.Length)
+        {
+            spriteRenderer.sprite = animationSprites[animationFrame];
+        }
+    }
+}
